9dcfe2c4007d48ca28d8c6ab515a1a4259895866
Tray Torrance <devwork@warrentorrance.com>
2012-03-10 12:10:18 -0800

    Add template for pinning

--- manifests/pin.pp
+++ manifests/pin.pp
@@ -1,11 +1,12 @@
 define apt::pin( $glob = $name, $release = 'a=testing', $priority = '700' ) {
   file {
     "apt_pinning_${name}":
-      path   => "/etc/apt/preferences.d/${name}.conf",
-      ensure => present,
-      owner  => 'root',
-      group  => 'root',
-      mode   => 0644;
+      path    => "/etc/apt/preferences.d/${name}.conf",
+      ensure  => present,
+      owner   => 'root',
+      group   => 'root',
+      mode    => 0644,
+      content => template( 'apt/pin.conf.erb' );
   }
 }
 
