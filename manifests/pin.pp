d8277b5fb6e25708a3264b007c70f721a05abd32
Tray Torrance <devwork@warrentorrance.com>
2012-03-10 13:01:02 -0800

    Fine-tune apt-pinning

--- manifests/pin.pp
+++ manifests/pin.pp
@@ -1,12 +1,14 @@
 define apt::pin( $glob = $name, $release = 'a=testing', $priority = '700' ) {
   file {
     "apt_pinning_${name}":
-      path    => "/etc/apt/preferences.d/${name}",
       ensure  => present,
+      path    => "/etc/apt/preferences.d/${name}",
       owner   => 'root',
       group   => 'root',
       mode    => 0644,
-      content => template( 'apt/pin.erb' );
+      content => template( 'apt/pin.erb' ),
+      notify  => Exec['update package list'],
+      require => File['preferences.d'];
   }
 }
 
