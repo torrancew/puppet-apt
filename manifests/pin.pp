35bf8bac11340da8fe38bf944cc1bebd5a477446
Tray Torrance <devwork@warrentorrance.com>
2012-03-10 12:55:34 -0800

    Fix pin file extensions

--- manifests/pin.pp
+++ manifests/pin.pp
@@ -1,12 +1,12 @@
 define apt::pin( $glob = $name, $release = 'a=testing', $priority = '700' ) {
   file {
     "apt_pinning_${name}":
-      path    => "/etc/apt/preferences.d/${name}.conf",
+      path    => "/etc/apt/preferences.d/${name}",
       ensure  => present,
       owner   => 'root',
       group   => 'root',
       mode    => 0644,
-      content => template( 'apt/pin.conf.erb' );
+      content => template( 'apt/pin.erb' );
   }
 }
 
