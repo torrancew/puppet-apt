fe9a5bd8c5474837283ae3e07a3cdee75e383cc7
Tray Torrance <devwork@warrentorrance.com>
2013-07-07 23:56:55 -0700

    Update pin to use title instead of name

--- manifests/pin.pp
+++ manifests/pin.pp
@@ -1,14 +1,13 @@
-define apt::pin( $glob = $name, $release = 'a=testing', $priority = '450' ) {
+define apt::pin( $glob = $title, $release = 'a=testing', $priority = '450' ) {
   file {
-    "apt_pinning_${name}":
+    "apt_pinning_${title}":
       ensure  => present,
-      path    => "/etc/apt/preferences.d/${name}",
+      path    => "/etc/apt/preferences.d/${title}",
       owner   => 'root',
       group   => 'root',
       mode    => 0644,
       content => template( 'apt/pin.erb' ),
       notify  => Exec['update package list'],
-      require => File['preferences.d'];
   }
 }
 
