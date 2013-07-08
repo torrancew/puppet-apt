d6e0e74f505495e9620baab08630a56809439574
Tray Torrance <devwork@warrentorrance.com>
2013-07-07 23:56:42 -0700

    Update key define to use title instead of name

--- manifests/key.pp
+++ manifests/key.pp
@@ -1,12 +1,11 @@
-define apt::key( $key_id = $name, $key_path = '', $key_server = '' ) {
+define apt::key( $key_id = $title, $key_path = '', $key_server = '' ) {
   include apt
-  include stdlib
 
   $default_keyserver = 'keys.gnupg.net'
 
   if empty( $key_path ) {
     exec {
-      "add remote ${name} apt key":
+      "add remote ${title} apt key":
         command => empty( $key_server ) ? {
           true  => "apt-key adv --keyserver $default_keyserver --recv-key $key_id",
           false => "apt-key adv --keyserver $key_server --recv-key $key_id",
@@ -20,13 +19,13 @@ define apt::key( $key_id = $name, $key_path = '', $key_server = '' ) {
 
   else {
     exec {
-      "add ${name} apt key from file":
+      "add ${title} apt key from file":
         command => $key_path ? {
-          /^http:/ => "wget -O- $key_path | gpg --no-default-keyring --keyring /etc/apt/trusted.gpg.d/${name}.gpg --import -",
-          default  => "gpg --no-default-keyring --keyring /etc/apt/trusted.gpg.d/${name}.gpg --import ${key_path}",
+          /^http:/ => "wget -O- $key_path | gpg --no-default-keyring --keyring /etc/apt/trusted.gpg.d/${title}.gpg --import -",
+          default  => "gpg --no-default-keyring --keyring /etc/apt/trusted.gpg.d/${title}.gpg --import ${key_path}",
         },
         path    => [ '/usr/local/bin', '/usr/local/sbin', '/usr/bin', '/usr/sbin', '/bin', '/sbin' ],
-        creates => "/etc/apt/trusted.gpg.d/${name}.gpg",
+        creates => "/etc/apt/trusted.gpg.d/${title}.gpg",
         notify  => Exec['update package list'],
         require => File['trusted.gpg.d'];
     }
