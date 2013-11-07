4bb9888dc3ff314cbb05f8b20a2e6c191de93a96
Tray Torrance <devwork@warrentorrance.com>
2013-11-06 20:51:34 -0800

    Refactor apt::source to enforce an $ensure parameter

--- manifests/source.pp
+++ manifests/source.pp
@@ -1,5 +1,50 @@
+# == Define: apt::source
+#
+# A defined type for managing apt sources in a robust fashion
+#
+# === Parameters:
+#
+# [*ensure*]
+#   The state to enforce for this source
+#   Valid values: present, absent
+#   Default: present
+#
+# [*url*]
+#   The base URL for this source
+#
+# [*release*]
+#   The debian release to search for on this source
+#   Default: $::lsbdistcodename
+#
+# [*components*]
+#   An array of components to search for on this source
+#   Default: ['main']
+#
+# [*source*]
+#   A boolean that controls whether or not source packages
+#   should be indexed from this source
+#   Default: false
+#
+# === Variables:
+#
+# None
+#
+# === Examples:
+#
+# None
+#
+# === Authors:
+#
+# * Tray Torrance
+#
+# === Copyright:
+#
+# Copyright 2013 Tray Torrance
+# unless otherwise noted.
+#
 define apt::source(
   $url,
+  $ensure     = present,
   $release    = $::lsbdistcodename,
   $components = [ 'main' ],
   $source     = false,
@@ -8,9 +53,9 @@ define apt::source(
 
   file {
     "${title}.list":
-      ensure  => present,
+      ensure  => $ensure,
       path    => "/etc/apt/sources.list.d/${title}.list",
-      mode    => 0644,
+      mode    => '0644',
       content => template( 'apt/source.list.erb' );
   }
 }
