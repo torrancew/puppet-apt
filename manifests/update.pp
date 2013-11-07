0736358638de2543f2f0dabd9527045fd79f9929
Tray Torrance <devwork@warrentorrance.com>
2013-11-06 20:57:10 -0800

    Document apt::update

--- manifests/update.pp
+++ manifests/update.pp
@@ -1,8 +1,35 @@
+# == Class: apt::update
+#
+# A class for managing apt updates
+#
+# === Parameters
+#
+# None
+#
+# === Variables:
+#
+# None
+#
+# === Examples:
+#
+# None
+#
+# === Authors:
+#
+# * Tray Torrance
+#
+# === Copyright:
+#
+# Copyright 2013, Tray Torrance
+# unless otherwise noted.
+#
 class apt::update {
+  Class['apt'] -> Class['apt::update']
+
   exec {
     'update package list':
       command     => 'apt-get update',
-      refreshonly => true,
+      refreshonly => true;
   }
 }
 
